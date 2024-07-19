790bed4e0a343440110003bbdf2b2298a927c67a
Abdulrahman843 <energyspur@gmail.com>
2024-07-19 12:13:03 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log("Hello World!");
+
+var state = {
+  taskList: [
+    {
+      imageURL: "",
+      taskTitle: "",
+      taskType: "",
+      taskDescription: "",
+    },
+    {
+      imageURL: "",
+      taskTitle: "",
+      taskType: "",
+      taskDescription: "",
+    },
+    {
+      imageURL: "",
+      taskTitle: "",
+      taskType: "",
+      taskDescription: "",
+    },
+    {
+      imageURL: "",
+      taskTitle: "",
+      taskType: "",
+      taskDescription: "",
+    },
+  ],
+};
